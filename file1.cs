5236bb823403844a303ce9f467e512466a5fdfdc
Harry <harry@grinningwizards.com>
2020-01-24 20:07:20 -0500

    Added Master Blaster

--- file1.cs
+++ file1.cs
@@ -1,6 +1,7 @@
 class Program {
     public static void main() {
         var x = "Hello World";
-        var z = "master";
+        var z = "master blaster";
+        var q = "is the man";
     }
 }
